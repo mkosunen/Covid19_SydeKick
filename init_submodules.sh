378010bd14846292ceadd5464bc47e274d1b63d4
Marko Kosunen <marko.kosunen@aalto.fi>
2020-03-20 19:21:46 +0200

    Cleanup

--- init_submodules.sh
+++ init_submodules.sh
@@ -3,16 +3,8 @@
 #Written by by Marko Kosunen, marko.kosunen@aalto.fi, 2017
 DIR=$( cd `dirname $0` && pwd )
 SUBMODULES="\
-    ./Entities/rtl \
-    ./Entities/eldo \
     ./Entities/thesdk \
-    ./Entities/thesdk_helpers \
-    ./Entities/inverter \
-    ./Entities/inv_sim \
-    ./Simulations/Slidetemplate \
-    ./Simulations/Simtemplate \
-    ./Simulations/Inverter"
-
+    ./Entities/thesdk_helpers"
 git submodule sync
 for mod in $SUBMODULES; do 
     git submodule update --init $mod

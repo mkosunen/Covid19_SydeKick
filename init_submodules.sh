e4372510ae6f87b40fe44fb1498be17d02c6c167
Marko Kosunen <marko.kosunen@aalto.fi>
2020-03-21 00:02:34 +0200

    Add covid19 submodule

--- init_submodules.sh
+++ init_submodules.sh
@@ -3,6 +3,7 @@
 #Written by by Marko Kosunen, marko.kosunen@aalto.fi, 2017
 DIR=$( cd `dirname $0` && pwd )
 SUBMODULES="\
+    ./Entities/covid19 \
     ./Entities/thesdk \
     ./Entities/thesdk_helpers"
 git submodule sync
